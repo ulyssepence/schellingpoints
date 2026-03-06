afdedad280591ee02b9aaf9aca512460012d7235
Ulysse Pence <git@ulyssepence.com>
2026-03-06 15:44:43 -0200

    Testing shake

--- src/client/shake.ts
+++ src/client/shake.ts
@@ -2,8 +2,7 @@ import { Capacitor } from '@capacitor/core'
 
 export function initShake(onShake: () => void) {
   if (!Capacitor.isNativePlatform()) return
-  import('@capgo/capacitor-shake').then(async ({ Shake }) => {
-    await Shake.start({ threshold: 3.5 })
-    Shake.addListener('shake', onShake)
+  import('@capgo/capacitor-shake').then(({ CapacitorShake }) => {
+    CapacitorShake.addListener('shake', onShake)
   })
 }
