4ada2be327ada007897bccf410b66a9081f99286
Ulysse Pence <git@ulyssepence.com>
2026-03-06 15:04:36 -0200

    Testing shake

--- src/client/shake.ts
+++ src/client/shake.ts
@@ -3,6 +3,12 @@ import { Capacitor } from '@capacitor/core'
 export function initShake(onShake: () => void) {
   if (!Capacitor.isNativePlatform()) return
   import('@capgo/capacitor-shake').then(({ Shake }) => {
-    Shake.addListener('shake', onShake)
+    console.log('[shake] registering listener')
+    Shake.addListener('shake', () => {
+      console.log('[shake] shake detected')
+      onShake()
+    })
+  }).catch(err => {
+    console.error('[shake] failed to load plugin:', err)
   })
 }
